4ae575b95d5151d130327a5080aef3afd625e341
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-03-10 16:52:32 +0900

    starting demo challenge

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SideBar from "./components/SideBar.jsx";
+
+function App() {
+  return (
+    <>
+      <SideBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+export default function SideBar() {
+  return (
+    <section className="fixed flex flex-col items-center gap-4 my-4 bg-stone-700 w-1/4 h-full p-8 rounded">
+      <label className="text-sm font-bold uppercase text-white">
+        Your Projects
+      </label>
+      <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-500 text-stone-100 hover:bg-stone-600 hover:text-stone-100">
+        + Add Project
+      </button>
+    </section>
+  );
+}
